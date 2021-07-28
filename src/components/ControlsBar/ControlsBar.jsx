ec4b94458908206cdc8e079eea2f764d88ff64e6
adithyavis <adithyaviswam@gmail.com>
2021-07-29 03:45:47 +0900

    Add reset game logic

--- src/components/ControlsBar/ControlsBar.jsx
+++ src/components/ControlsBar/ControlsBar.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 
 import Button from 'react-bootstrap/Button';
 
+import { useGameInfo } from 'providers/GameInfoProvider';
+import { useStats } from 'providers/StatsProvider';
+
 function Header(props) {
   const { setShowHistoryModal } = props;
 
+  const { setShowStartPopup } = useGameInfo();
+  const { resetStats } = useStats();
+
   return (
     <div className="controls-bar d-flex align-items-center p-3">
-      <Button variant="primary">Reset</Button>
+      <Button
+        variant="primary"
+        onClick={() => {
+          resetStats();
+          setShowStartPopup(true);
+        }}
+      >
+        Reset
+      </Button>
       <Button
         variant="primary"
         className="history-button"
