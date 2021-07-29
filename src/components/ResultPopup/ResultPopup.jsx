e38c92ef89f193db0b30c83ca5ef4082776a4c32
adithyavis <adithyaviswam@gmail.com>
2021-07-29 15:47:45 +0900

    Modify Result Popup to show continue game message

--- src/components/ResultPopup/ResultPopup.jsx
+++ src/components/ResultPopup/ResultPopup.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 
 import { useGameInfo } from 'providers/GameInfoProvider';
+
+import levelsConfig from 'constants/levelsConfig';
 
 function VictoryDefeatPopup() {
   const {
@@ -14,9 +16,27 @@ function VictoryDefeatPopup() {
     setShowDefeatPopup,
   } = useGameInfo();
 
+  const [showContinueLevel, setShowContinueLevel] = useState(null);
+
+  useEffect(() => {
+    if (!showStartPopup) {
+      return;
+    }
+    const previouslySavedLevel = parseInt(window.localStorage.getItem('level'));
+    if (previouslySavedLevel) {
+      const found = levelsConfig.allLevels.find(
+        (level) => level === previouslySavedLevel
+      );
+      if (found) {
+        setShowContinueLevel(found);
+      }
+    }
+  }, [showStartPopup]);
+
   const handleClickStartGame = () => {
     if (showStartPopup) {
       setShowStartPopup(false);
+      setShowContinueLevel(null);
     } else if (showVictoryPopup) {
       window.localStorage.setItem('level', null);
       setShowVictoryPopup(false);
@@ -32,9 +52,17 @@ function VictoryDefeatPopup() {
       <div className="result-popup d-flex flex-column align-items-center justify-content-center">
         {showVictoryPopup && <div>You have won!</div>}
         {showDefeatPopup && <div>You have lost!</div>}
+        {showStartPopup && <div>Memory: Card Game</div>}
         <Button variant="primary" onClick={handleClickStartGame}>
-          {showStartPopup ? 'Start Game' : 'Start again'}
+          {showStartPopup
+            ? showContinueLevel === null
+              ? 'Start Game'
+              : 'Continue Game'
+            : 'Start again'}
         </Button>
+        {showContinueLevel !== null && (
+          <div className="h6">Last saved: Level {showContinueLevel}</div>
+        )}
       </div>
     );
   }
